6bde2b4e8f7df1de60d92d058eb6dd1b4afce0bf
Ewan <646067@student.cavc.ac.uk>
2019-09-09 09:41:06 +0100

    Commit 1

--- lessonOne.cpp
+++ lessonOne.cpp
@@ -7,6 +7,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	//Variables to contain the numbers for the calculation
+	int num1 = 0;
+	int num2 = 0;
+	int ans = 0;
+
+	//Asks for the first number
+    cout << "Please type in the first number: ";
+	cin >> num1;
+	cout << "\n";
+
+	//Asks for the second number
+	cout << "Please type in the second number: ";
+	cin >> num2;
+	cout << "\n";
+
+	//Does the calculation
+	ans = num1 * num2;
+
+	//Outputs the answer
+	cout << "The answer is " << ans << ".\n"
 	
 }
