b68327eb88983453160c5145f803968f8739f37d
Ewan <646067@student.cavc.ac.uk>
2019-09-09 13:21:49 +0100

    Finished FileOptions

--- lessonOneFileOptions.cpp
+++ lessonOneFileOptions.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
 	//Variable to hold the users choice
-	string choice = " ";
+	string choice;
 
 	//While loop to keep the menu going until they type "exit"
 	while (choice != "Exit") {
@@ -19,18 +19,18 @@ int main()
 		cin >> choice;
 
 		//If statement to pick options
-		if (choice == "Open file" or choice == "open file") {
-			cout << "You have selected to open a file";
+		if (choice == "Open file") {
+			cout << "You have selected to open a file\n";
 		}
-		else if (choice == "Save file" or choice == "save file") {
-			cout << "You have selected to save a file";
+		else if (choice == "Save file") {
+			cout << "You have selected to save a file\n";
 		}
-		else if (choice == "Exit program" or choice == "exit program" or choice == "Exit" or choice == "exit") {
-			cout << "You have selected to exit";
+		else if (choice == "Exit program") {
+			cout << "You have selected to exit\n";
 			return 0;
 		}
 		else {
-			cout << "Option not recognised, please try again";
+			cout << "Option not recognised, please try again\n";
 		}
 	}
 }
