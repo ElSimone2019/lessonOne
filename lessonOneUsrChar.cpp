4470555817e5a567136bd9fba437a5a6652af93b
Ewan <646067@student.cavc.ac.uk>
2019-09-10 08:56:37 +0100

    New Activity File Added

---
+++ lessonOneUsrChar.cpp
@@ -0,0 +1,18 @@
+// lessonOne.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <string>
+
+//Stops the need to add "std" in front of statements
+using namespace std;
+
+int main()
+{
+	//Variable to store user character
+	char usrChar = 'a';
+
+	//Input for user character
+	cout << "Please enter a letter: ";
+	cin >> usrChar;
+	cout << "\n";
+}
